e8911c0ef5435304a5302b3ae97a16664fd8c10a
vagrant <vagrant@rails-dev-box>
2014-07-22 18:00:39 +0000

    Created rails app and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+#Blocmarks
+##An application allowing users to bookmark URLs via email, peruse other user's bookmarks and maintain a personal index of categorized bookmarks
+####This is my fourth app that I have built during Bloc
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocmarks::Application.config.secret_key_base = 'd29d3229b7dabd950b0644aef750e391df5b276dafe01ca9395b4bd925fde57550a2c5b1d6bfed3fcbdcee44c5e34326f082acd1f2d54ecdf92dd1d2fd4eeada'
