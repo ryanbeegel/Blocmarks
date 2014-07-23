1b382af76be3c2c512d3d2b3eeb162ab1ec4cb72
vagrant <vagrant@rails-dev-box>
2014-07-23 20:47:55 +0000

    Made facebook authorization capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.skip_confirmation!
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      #user.image = auth.info.image # assuming the user model has an image
     end
   end
 
