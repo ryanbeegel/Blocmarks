c48d466f836ca3a783b320e2c254d1f14624f71a
vagrant <vagrant@rails-dev-box>
2014-07-25 19:43:37 +0000

    Added mailgun for bookmark emailing capability

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,8 @@
+class IncomingController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:create]
+
+  def create
+    puts "INCOMING PARAMS HERE: #{params}"
+    head 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Blocmarks::Application.routes.draw do
+  get "incoming/create"
+  resources :names
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, only: [:update]
   get "welcome/index"
 
   root to: 'welcome#index'
+
+  post :incoming, to: 'incoming#create'
   
   # devise_scope :user do
   #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
